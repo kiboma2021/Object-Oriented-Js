a3232e3c5713b4af019cb6e68fe549083a828fb5
Ben Kiboma <omayioben@gmail.com>
2022-06-07 22:05:44 +0300

    JS and index files

---
+++ index.js
@@ -0,0 +1,9 @@
+class User {
+  constructor (name,email) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+var userOne = new User("Ben","omayioben@gmail.com");
+var userTwo = new User("Ann","ann@gmail.com");
